f48a247a9ead83596e134938edc10727be85a171
Mike Stephen Gollena <55428756+DJmistMAGO@users.noreply.github.com>
2023-11-07 16:06:29 +0800

    some chnages

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -5,22 +5,24 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Admin Login</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('../assets/css/tailwind.output.css') }}" />
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <script src="{{ asset('../assets/js/init-alpine.js') }}"></script>
+    {{-- <script src="{{  }}"></script> --}}
 </head>
 
 <body>
     <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
-        <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden rounded-lg bg-white rounded-lg shadow-xl dark:bg-gray-800">
+        <div
+            class="flex-1 h-full max-w-4xl mx-auto overflow-hidden rounded-lg bg-white rounded-lg shadow-xl dark:bg-gray-800">
             <div class="flex flex-col overflow-y-auto md:flex-row">
                 <div class="h-32 md:h-auto md:w-1/2">
-                    <img aria-hidden="true" class="object-cover w-full h-full dark:hidden"
-                        src="../assets/img/doc1.jpg" alt="Office" />
-                    <img aria-hidden="true" class=" object-cover w-full h-full dark:block"
-                        src="../assets/img/doc1.jpg" alt="Office" />
+                    {{-- <img aria-hidden="true" class="object-cover w-full h-full dark:hidden" src="../assets/img/doc1.jpg"
+                        alt="Office" /> --}}
+                    <img aria-hidden="true" class=" object-cover w-full h-full dark:block" src="../assets/img/doc1.jpg"
+                        alt="Office" />
                 </div>
                 <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
                     <div class="w-full">
@@ -43,7 +45,6 @@
                                     required autocomplete="current-password" />
                             </label>
 
-
                             <!-- You should use a button here, as the anchor is only used for the example  -->
                             <button
                                 class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-blue-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
@@ -51,7 +52,6 @@
                                 Log in
                             </button>
                         </form>
-
 
                     </div>
                 </div>
